dffb124e2271fd973b87d05b84bb6618d3dd3f32
wangdian <36665764+wangdianyh@users.noreply.github.com>
2022-09-13 11:21:15 -0300

    update title

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
 <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid container">
-            <a class="navbar-brand" href="#">Assignment</a>
+            <a class="navbar-brand" href="#">Pockemon User</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
